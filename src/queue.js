2c3af9bea257b2f07e2ba2633605c48f13e5968d
YA <vector_cp@mail.ru>
2022-05-01 20:27:55 +0300

    feat: basic-js-ds=> queue: ok

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getUnderlyingList() {
+   return this.head
+  }
+
+  enqueue( value ) {
+   const node = new ListNode(value)
+   if (this.head) {
+    this.tail.next = node
+    this.tail = node
+     
+   } else {
+    this.head = node
+    this.tail = node
+
+   }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!this.head) {
+      return null;
+    }
+
+   const current = this.head
+   this.head = this.head.next
+   return current.value
   }
 }
 
